a7bb974b7247f7023b7b64ab9eba72fa90bc08e2
Michael Lieber <m.lieber@posteo.de>
2023-04-20 14:55:39 +0200

    removed express.static

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
-import cors from 'cors'
+import cors from "cors";
 import express from "express";
-import bodyParser  from 'body-parser';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import path from "path";
 import { fileURLToPath } from "url";
-import router from './routes/userroute.js'
-
+import router from "./routes/userroute.js";
 
 dotenv.config();
 
-const app=express();
+const app = express();
 app.use(cors());
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
-const port=process.env.PORT || '4000'
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || "4000";
 
-app.use('/',router);
+app.use("/", router);
 const mongo_URI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`;
-mongoose.connect(mongo_URI,{ dbName:process.env.DB_NAME})
-.then( ()=>{
+mongoose
+  .connect(mongo_URI, { dbName: process.env.DB_NAME })
+  .then(() => {
     console.log(`Database connected 😎`);
-})
-.catch(err =>{
+  })
+  .catch((err) => {
     console.log(err);
+  });
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+app.listen(port, () => {
+  console.log(`listening to ${port}  port`);
 });
- const __filename = fileURLToPath(import.meta.url);
-
- const __dirname = path.dirname(__filename);
-
-
-
-app.use(express.static(path.join(__dirname, "../client/build")));
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname + "/../client/build/index.html"))
-})
-
-
-app.listen(port,()=>{
-    console.log(`listening to ${port}  port`);
-})
